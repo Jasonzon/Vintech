367f40a27fc46f06cb993c0d17a5b2ec44604d32
jason.moret <jazzmoret@gmail.com>
2022-08-30 01:13:10 +0200

    route dm

--- routes/dm.js
+++ routes/dm.js
@@ -1,35 +1,11 @@
 const router = require("express").Router()
 const pool = require("../db")
 
-router.get("/", async (req,res) => {
+router.get("/:from&:to&:bottom&:top", async (req,res) => {
     try {
-        const allDms = await pool.query("select * from dm")
-        res.json(allDms.rows)
-    }
-    catch (err) {
-        console.log(err.message)
-    }
-})
-
-router.get("/:id", async (req,res) => {
-    try {
-        const {id} = req.params
-        const article = await pool.query("select * from article where article_id = $1",[id])
-        if (article.rows.length === 0) {
-            return res.status(403).send("Not Authorized")
-        }
-        else {
-            res.json(article.rows[0])
-        }
-    } catch (err) {
-        console.log(err.message)
-    }
-})
-
-router.get("/polyuser/:id", async (req,res) => {
-    try {
-        const {id} = req.params
-        const allArticles = await pool.query("select * from article where article_polyuser = $1",[id])
+        const {from,to,bottom,top} = req.params
+        const allDms = await pool.query("select * from dm where (dm_from = $1 and dm_to = $2) or (dm_from = $2 and dm_to = $1)",[from,to])
+        res.json(allDms.rows.filter((dm,index) => bottom <= index <= top))
     } catch (err) {
         console.log(err.message)
     }
@@ -37,9 +13,9 @@ router.get("/polyuser/:id", async (req,res) => {
 
 router.post("/", async (req,res) => {
     try {
-        const {pic,title,description,polyuser,price,city} = req.body
-        const newArticle = await pool.query("INSERT INTO article (article_name, article_polyuser, article_price, article_city, article_pic, article_description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", [title, polyuser, price, city, pic, description])
-        if (newArticle.rows.length === 0) {
+        const {message,from,to} = req.body
+        const newDm = await pool.query("INSERT INTO dm (dm_message, dm_from, dm_to) VALUES ($1, $2, $3) RETURNING *", [message,from,to])
+        if (newDm.rows.length === 0) {
             return res.status(403).send("Not Authorized")
         }
         else {
@@ -50,27 +26,11 @@ router.post("/", async (req,res) => {
     }
 })
 
-router.put("/:id", async (req, res) => {
-    try {
-        const {id} = req.params
-        const {pic,title,description,polyuser,price,city} = req.body
-        const updateArticle = await pool.query("UPDATE article SET article_title = $2, article_price = $3, article_pic = $4, article_description = $5, article_city = $6, article_polyuser = $7 WHERE article_id = $1 RETURNING *",[id, title, price, pic, description, city, polyuser])
-            if (updateArticle.rows.length === 0) {
-                return res.status(403).send("Not Authorized")
-            }
-            else {
-                res.json(updateArticle.rows[0])
-            }
-    } catch (err) {
-        console.log(err.message)
-    }
-})
-
 router.delete("/:id", async (req,res) => {
     try {
         const {id} = req.params
-        const deleteArticle = await pool.query("DELETE FROM article WHERE article_id = $1",[id])
-        if (deleteArticle.rows.length === 0) {
+        const deleteDm = await pool.query("DELETE FROM dm WHERE dm_id = $1",[id])
+        if (deleteDm.rows.length === 0) {
             return res.status(403).send("Not Authorized")
         }
         else {
