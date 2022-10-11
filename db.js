5c4569183991c49d2e03dc92ff630436b65f605e
Jason <jazzmoret@gmail.com>
2022-10-11 14:47:30 +0200

    mise en place

--- db.js
+++ db.js
@@ -5,21 +5,24 @@ require("dotenv").config()
 
 const proConfig = process.env.DATABASE_URL
 
-const pool = process.env.NODE_ENV !== "production" ? 
+console.log(process.env.PG_USER)
+console.log(process.env.PG_PASSWORD)
+console.log(process.env.PG_DATABASE)
+console.log(process.env.PG_PORT)
+console.log(process.env.PG_HOST)
+
+const pool = //process.env.NODE_ENV !== "production" ? 
 new Pool ({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD.toString(),
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
   host: process.env.PG_HOST
-})
-
-:
-new Pool({
+}) /*: new Pool({
     connectionString:proConfig,
     ssl: {
         rejectUnauthorized: false
       }
-})
+})*/
 
 module.exports = pool
