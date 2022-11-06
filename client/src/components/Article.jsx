2fb5cb14bab8727e4290619a17d1f40c8df1bc06
Jason <jazzmoret@gmail.com>
2022-11-06 15:31:04 +0100

    font size sell

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 function Article({id,pic, title, description, created_at, price, city, polyuser}) {
     return (
         <Link to={`/view/${id}`}>
-            <div className="article" style={{borderColor:`var(--${city.toLowerCase()})`}}>
+            <div className="article" style={{color:`var(--${city.toLowerCase()})`}}>
                 <h3>{title}</h3>
                 <img alt={title} src={pic} />
                 <h3>{price}</h3>
