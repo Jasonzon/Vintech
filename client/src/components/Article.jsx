3fe42f1db831dc006960f0f80527b40fa9259c8c
Jason <jazzmoret@gmail.com>
2022-11-07 12:45:51 +0100

    problemes css grid

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,15 +1,14 @@
 import "../styles/Article.css"
-import {Link} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 
 function Article({id,pic, title, description, created_at, price, city, polyuser}) {
+    const navigate = useNavigate()
     return (
-        <Link to={`/view/${id}`}>
-            <div className="article" style={{color:`var(--${city.toLowerCase()})`}}>
+            <div onClick={() => navigate(`/view/${id}`)} className="article" style={{color:`var(--${city.toLowerCase()})`}}>
                 <h3>{title}</h3>
                 <img alt={title} src={pic} />
                 <h3>{price}</h3>
             </div>
-        </Link>
     )
 }
 
