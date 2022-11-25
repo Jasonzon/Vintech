60ddd1b54023a65e4c0a215cdc6c19622efdab02
Jason <jazzmoret@gmail.com>
2022-11-25 13:55:10 +0100

    links homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,42 +12,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import TextField from "@mui/material/TextField";
-
-/*
-
-    return (
-        <div className="home">
-            <div className="filter">
-                <label>Rechercher</label>
-                <input name="search" className="search" />
-            </div>
-            <Container>
-                <Row>
-                {articles.map(({article_id,article_pic, article_title, article_description, created_at, article_polyuser, article_price, article_city},index) => 
-                    <Col>
-                        <Article key={article_title}
-                            pic={article_pic}
-                            title={article_title}
-                            description={article_description}
-                            created_at={created_at}
-                            polyuser={article_polyuser}
-                            price={article_price}
-                            city={article_city}
-                            id={article_id}
-                        />
-                    </Col>
-                )}
-                </Row>
-            </Container>
-        </div>
-    )
-}
-
-export default Home
-
-*/
+import {useNavigate} from "react-router-dom"
   
 function Home() {
+
+  const navigate = useNavigate()
 
   const [articles,setArticles] = useState([])
 
@@ -72,7 +41,7 @@ function Home() {
         <Grid container spacing={2}>
           {articles.filter((article) => article.article_title.toLowerCase().includes(search.toLowerCase()) || article.article_description.toLowerCase().includes(search.toLowerCase())).map(({article_id,article_pic, article_title, article_description, created_at, article_polyuser, article_price, article_city},index) => (
             <Grid item key={index} xs={12} sm={6} md={4}>
-              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}>
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', cursor:"pointer"}} onClick={() => navigate(`/view/${article_id}`)}>
                 <CardMedia component="img" image={article_pic} alt={article_title} style={{objectFit: "contain",maxHeight:200}}/>
                 <CardContent sx={{ flexGrow: 1 }}>
                   <Typography gutterBottom variant="h5" component="h2">{article_title}</Typography>
