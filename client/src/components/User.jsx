fd84c5e0e91a135c9fa394107de87e6490d63ab8
Jason <jazzmoret@gmail.com>
2022-11-29 12:57:15 +0100

    avancement user

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -6,9 +6,16 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Home from "./Home"
+import Grid from "@mui/material/Grid"
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import MenuItem from "@mui/material/MenuItem"
+import Select from "@mui/material/Select"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
 
-
-/*
 function User({user, setUser}) {
 
     function logout() {
@@ -16,21 +23,9 @@ function User({user, setUser}) {
         setUser({})
     }
 
-    const [articles, setArticles] = useState([])
     const [modify, setModify] = useState(false)
+
     const [inputs, setInputs] = useState({pseudo:user.polyuser_name,description:user.polyuser_description,city:user.polyuser_city})
-
-    async function getArticles() {
-        const res = await fetch(`http://localhost:5500/article/polyuser/${user.polyuser_id}`, {
-            method: "GET"
-        })
-        const parseRes = await res.json()
-        setArticles(parseRes)
-    }
-
-    useEffect(() => {
-        getArticles()
-    },[])
 
     async function update() {
         if (inputs.pseudo !== "") {
@@ -47,76 +42,6 @@ function User({user, setUser}) {
     }
 
     return (
-
-        <div className="user">
-            <div className="left">
-                <div className="top">
-                    <img alt="photo de profil" src="https://media.istockphoto.com/vectors/default-avatar-profile-icon-vector-vector-id1337144146?b=1&k=20&m=1337144146&s=170667a&w=0&h=ys-RUZbXzQ-FQdLstHeWshI4ViJuEhyEa4AzQNQ0rFI=" />
-                    <div className="pseudo">
-                    {modify ? <input maxLength="20" className="user1" value={inputs.pseudo} onChange={(e) => setInputs({pseudo:e.target.value.replace(/[^a-zA-Z0-9]/g,'').replace(/\s+/g, ''), description:inputs.description, city:inputs.city})}/> : <h2>{user.polyuser_name}</h2>}
-                        <div className="bar"></div>
-                    </div>
-                </div>
-                <div className="bottom">
-                    <div className="modify">
-                        {modify ? <button style={{backgroundColor:`var(--${user.polyuser_city.toLowerCase()})`}} title="annuler" onClick={() => {setModify(false);setInputs({pseudo:user.polyuser_name,description:user.polyuser_description,city:user.polyuser_city})}}>Annuler</button> : <button style={{backgroundColor:`var(--${user.polyuser_city.toLowerCase()})`}} onClick={() => setModify(true)}>Modifier</button>}
-                        {modify ? <button style={{backgroundColor:`var(--${user.polyuser_city.toLowerCase()})`}} title="valider" onClick={() => update()}>Valider</button> : null}
-                    </div>
-                    <div className="ville">
-                    {modify ? <select required onChange={(e) => setInputs({pseudo:inputs.pseudo, city:e.target.value, description:inputs.description})} value={inputs.city} type="text" id="epolytech" name="epolytech">
-                     <option value="" disabled selected>Choisir une ville</option>
-                         <option style={{backgroundColor:"var(--montpellier)"}} className="montpellier" value="Montpellier">Montpellier</option>
-                         <option style={{backgroundColor:"var(--savoie)"}} className="savoie" value="Savoie">Savoie</option>
-                         <option style={{backgroundColor:"var(--marseille)"}} className="marseille" value="Marseille">Marseille</option>
-                         <option style={{backgroundColor:"var(--nice)"}} className="nice" value="Nice">Nice</option>
-                         <option style={{backgroundColor:"var(--grenoble)"}} className="grenoble" value="Grenoble">Grenoble</option>
-                         <option style={{backgroundColor:"var(--lyon)"}} className="lyon" value="Lyon">Lyon</option>
-                         <option style={{backgroundColor:"var(--clermont)"}} className="clermont" value="Clermont">Clermont</option>
-                         <option style={{backgroundColor:"var(--sorbonne)"}} className="sorbonne" value="Sorbonne">Sorbonne</option>
-                         <option style={{backgroundColor:"var(--saclay)"}} className="saclay" value="Saclay">Saclay</option>
-                         <option style={{backgroundColor:"var(--lille)"}} className="lille" value="Lille">Lille</option>
-                         <option style={{backgroundColor:"var(--nancy)"}} className="nancy" value="Nancy">Nancy</option>
-                         <option style={{backgroundColor:"var(--tours)"}} className="tours" value="Tours">Tours</option>
-                         <option style={{backgroundColor:"var(--orleans)"}} className="orleans" value="Orleans">Orleans</option>
-                         <option style={{backgroundColor:"var(--angers)"}} className="angers" value="Angers">Angers</option>
-                         <option style={{backgroundColor:"var(--nantes)"}} className="nantes" value="Nantes">Nantes</option>
-                     </select> : <h3>{user.polyuser_city}</h3> }
-                        <div className="bar1"></div>
-                        <div className="bar2"></div>
-                    </div>
-                    <div className="description">
-                    {modify ? <input className="user3" maxLength="30" value={inputs.description} onChange={(e) => setInputs({pseudo:inputs.pseudo, description:e.target.value, city:inputs.city})}/> : <h3>{user.polyuser_description}</h3>}
-                        <div className="bar1"></div>
-                        <div className="bar2"></div>
-                    </div>
-                    {modify ? null : <button onClick={() => logout()} style={user.polyuser_name ? {backgroundColor:`var(--${user.polyuser_city.toLowerCase()})`} : {backgroundColor:"var(--basic)"}}>Se déconnecter</button> }
-                </div>
-            </div>
-            <div className="cut"></div>
-            <div className="right">
-                <h2>Articles en vente</h2>
-                <ul className="articles">
-                    {articles.map(({article_id,article_pic, article_title, article_description, created_at, article_polyuser, article_price, article_city}) => 
-                        <Article key={article_title}
-                            pic={article_pic}
-                            title={article_title}
-                            description={article_description}
-                            created_at={created_at}
-                            polyuser={article_polyuser}
-                            price={article_price}
-                            city={article_city}
-                            id={article_id}
-                        />
-                    )}
-                </ul>
-            </div>
-        </div>
-    )
-}
-*/
-
-function User({user, setUser}) {
-    return (
         <Container component="main">
             <Box
                 sx={{
@@ -126,31 +51,71 @@ function User({user, setUser}) {
                     alignItems: 'center',
                 }}
             >
-                <Avatar sx={{ bgcolor: `var(--${user.polyuser_city.toLowerCase()})` }} aria-label="recipe">{user.polyuser_name.slice(0,1).toUpperCase()}</Avatar>
-                <Typography component="h1" variant="h5">{user.polyuser_name}</Typography>
-                <Box component="form" noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Modifier</Button>
-                </Box>
+                {!modify ?
+                    <Card sx={{maxWidth:500, minWidth:300}}>
+                        <CardHeader avatar={<Avatar sx={{ bgcolor:  `var(--${user.polyuser_city.toLowerCase()})`  }} aria-label="recipe">{user.polyuser_name.slice(0,1).toUpperCase()}</Avatar>}
+                            title={<Typography variant="h5">{user.polyuser_name}</Typography>}
+                            subheader={user.polyuser_city}
+                        />
+                        <CardContent>
+                            <Typography variant="h6">{user.polyuser_mail}</Typography>
+                            <Typography variant="body2">{user.polyuser_description}</Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <Button onClick={() => setModify(true)} fullWidth variant="contained" sx={{ mb: 2 }}>Modifier</Button>
+                        </CardActions>
+                    </Card>  
+                    :
+                    <Card sx={{maxWidth:500, minWidth:300}}>
+                        <CardHeader avatar={<Avatar sx={{ bgcolor:  `var(--${user.polyuser_city.toLowerCase()})`  }} aria-label="recipe">{user.polyuser_name.slice(0,1).toUpperCase()}</Avatar>}
+                            title={<TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="pseudo"
+                                        label="Pseudo"
+                                        name="pseudo"
+                                        autoComplete="pseudo"
+                                        autoFocus
+                                        value={inputs.pseudo}
+                                        onChange={(e) => setInputs({inputs,pseudo:e.target.value})}
+                                    />}
+                            subheader={<FormControl fullWidth >
+                                <InputLabel id="demo-simple-select-label" shrink={true} >Ville</InputLabel><Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Ville"
+                                notched={true}
+                                value={inputs.city}
+                                onChange={(e) => setInputs({...inputs,city:e.target.value})}
+                            >
+                                {["Marseille","Montpellier","Grenoble","Nice","Clermont","Lyon","Savoie","Tours","Orleans","Lille","Nancy","Nantes","Angers","Sorbonne","Saclay"].map((ville) => 
+                                    <MenuItem style={{backgroundColor:`var(--${ville.toLowerCase()})`}} value={ville}>{ville}</MenuItem>
+                                )}
+                            </Select></FormControl>}
+                        />
+                        <CardContent sx={{pt:0}}>
+                            <Typography variant="h6">{user.polyuser_mail}</Typography>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="description"
+                                label="Description"
+                                name="description"
+                                autoComplete="description"
+                                autoFocus
+                                value={inputs.description}
+                                onChange={(e) => setInputs({inputs,description:e.target.value})}
+                            />
+                        </CardContent>
+                        <CardActions fullWidth>
+                            <Container sx={{display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+                                <Button onClick={() => setModify(false)} variant="contained" sx={{ mb: 2}}>Annuler</Button>
+                                <Button onClick={() => update()} variant="contained" sx={{ mb: 2 }}>valider</Button>
+                            </Container>
+                        </CardActions>
+                  </Card> }
             </Box>
             <Home prop="User" user={user} setUser={setUser}/>
         </Container>
